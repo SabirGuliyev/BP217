8b92c790feeed4389291a5800d81d58b6743d105
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-10-02 20:09:39 +0400

    Control structure added

---
+++ ConditionExample/ConditionExample/Program.cs
@@ -0,0 +1,244 @@
+﻿
+#region IfElse
+
+//byte age = 19;
+//string gender = "female";
+
+
+//if (age >= 18 && gender=="female")
+//{
+//    Console.WriteLine("Xosh geldiz");
+//}
+//else
+//{
+//    Console.WriteLine("Uygun deyilsiz");
+//}
+
+//if (age >= 18)
+//{
+//    if (gender == "female")
+//    {
+//        Console.WriteLine("Xosh geldiz");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Xanim ishci axtaririq");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine("Yashiniz uygun deyil");
+//}
+
+
+
+
+
+
+
+
+//ctrl+k+c
+//ctrl+k+u
+//if (age >= 18)
+//{
+//    Console.WriteLine("Ishe qebul oldunuz");
+
+//}
+//else if (age == 17)
+//{
+//    Console.WriteLine("Srajor kimi qala bilersiz");
+//}
+//else if (age == 16)
+//{
+//    Console.WriteLine("1 il sonra gelin");
+//}
+//else
+//{
+//    Console.WriteLine("Yashiniz uygun deyil");
+//}
+
+
+
+#endregion
+
+#region SwitchCase
+//int choice = 3;
+
+
+//switch (choice)
+//{
+//    case 1:
+//        Console.WriteLine("Az dili secildi");
+//        break;
+//    case 2:
+//        Console.WriteLine("Rus dili secildi");
+//        break;
+//    case 3:
+//        Console.WriteLine("Eng dili secildi");
+//        break;
+//    default:
+//        Console.WriteLine("Yanlish secim");
+//        break;
+
+//}
+
+//int choice = 3;
+//if (choice == 1)
+//{
+//    Console.WriteLine("Az dili secildi");
+//}
+//else if (choice == 2)
+//{
+//    Console.WriteLine("Rus dili secildi");
+//}
+//else if (choice == 3)
+//{
+//    Console.WriteLine("Eng dili secildi");
+//}
+//else
+//{
+//    Console.WriteLine("Yanlish secim");
+//}
+
+
+
+
+//string day="monday";
+
+//switch (day)
+//{
+//    case "monday":
+//    case "tuesday":
+//    case "wednesday":
+//        Console.WriteLine("ish gunudur");
+//        break;
+//    case "saturday":
+//    case "sunday":
+//        Console.WriteLine("qeyri");
+//        break;
+//}
+
+//switch (day)
+//{
+//    case "monday" or "tuesday" or "wednesday":
+
+//        Console.WriteLine("ish gunudur");
+//        break;
+//    case "saturday" or "sunday":
+//        Console.WriteLine("qeyri");
+//        break;
+//}
+
+
+
+#endregion
+
+//ctrl+k+s
+#region ClassWork
+
+//int num = 78;
+
+////int temp = num % 10;
+////int num2 = num / 10;
+//int sum = (num%10)+(num/10);
+
+//Console.WriteLine(sum);
+
+
+//int sum = 0;
+
+////sum=10    i 4
+
+//for (int i = 1; i < 100; i++)
+//{
+
+//    sum+= i;
+
+//}
+
+//Console.WriteLine(sum);
+
+#endregion
+
+#region For
+//int count = 0;
+
+//for(int i = 1; i < 100; i++)
+//{
+
+//    if (i%14==0)
+//    {
+//        //count++;
+//        Console.WriteLine(i);
+//    }
+
+//}
+
+
+//Console.WriteLine(count); 
+
+
+//for (int i = 100; i>=0; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for (; ; )
+//{
+//    Console.WriteLine("Salam");
+//}
+#endregion
+
+#region WhileDoWhile
+//string answer = "";
+
+
+//while (answer == "no")
+//{
+//    Console.WriteLine("Program cixmaq isteyirsinizmi?");
+//    answer = Console.ReadLine();
+//}
+
+//string answer = "";
+//do
+//{
+//    Console.WriteLine("Program cixmaq isteyirsinizmi?");
+//    answer = Console.ReadLine();
+//}
+//while (answer == "no");
+
+
+
+//int num = 0;
+
+//int count = 0;
+
+//while (num != 0)
+//{
+//    num /= 10;
+//    count++;
+//}
+
+//do
+//{
+//    num /= 10;
+//    count++;
+//} while (num!=0);
+
+
+//Console.WriteLine(count);
+
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
