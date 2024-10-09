19edc6618a5ccdfba510f770c2d7b767b36abc92
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-10-09 20:52:28 +0400

    Big o and Methods added

---
+++ BigOExample/BigOExample/Program.cs
@@ -0,0 +1,188 @@
+﻿
+
+//int num = 20000;
+
+
+
+//for (int i = 0; i < num; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//O(1)
+//if (num > 10)
+//{
+//    Console.WriteLine("yes");
+//}
+//else
+//{
+//    Console.WriteLine("no");
+//}
+
+
+
+//int[] arr = { 2, 4, 5, 6, 9, 11, 12,13, };
+
+
+//Console.WriteLine(arr[0]);
+
+
+//int num = 113;
+
+//int step = 0;
+//bool result=false;
+
+//for (int i = 2;i*i <= num; i++)
+//{
+//    step++;
+//    if (num % i == 0)
+//    {
+//       result = true;
+//        break;
+//    }
+//}
+
+//if (result)
+//{
+//    Console.WriteLine("Murekkeb");
+//}
+//else
+//{
+//    Console.WriteLine("Sade");
+//}
+//Console.WriteLine(step);
+
+
+//int num = 20;
+//int step = 0;
+//for (int i = 0; i < num; i++)
+//{
+//    for (int j = 0; j < num; j++)
+//    {
+//        step++;
+//    }
+//}
+
+
+//Console.WriteLine(step);
+
+
+
+
+
+//int[] nums = { 34, 12, 45, 4, 1, 8, 9 };
+//int search = 8;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == search)
+//    {
+//        Console.WriteLine("var");
+//        break;
+//    }
+//}
+
+
+//int[] arr = { 1, 4, 12, 20, 30, 45, 67, 90 };
+/////////////// L              L                 H
+/////
+//int step = 0;
+//int search = 4;
+
+
+//int low = 0;
+//int high = arr.Length - 1;
+
+//int mid = (low + high) / 2;
+
+//while (arr[mid] != search)
+//{
+//    step++;
+//    if (arr[mid] > search)
+//    {
+//        high = mid - 1;
+
+//    }
+//    else
+//    {
+//        low = mid + 1;
+
+//    }
+//    mid= (low + high) / 2;
+//}
+
+//Console.WriteLine("index: "+mid+" deyeri: " + arr[mid]);
+//Console.WriteLine("Addimlarin sayi :"+step);
+
+
+
+
+
+//int[] numbers = { 6, 5, 3, 60, 10,71, 0, 81,11,23,67,99,-5,-8 };
+
+//int step = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    for (int j = 0;j< numbers.Length-1-i; j++)
+//    {
+//        step++;
+//        if (numbers[j] > numbers[j + 1])
+//        {
+
+//            int save = numbers[j];
+//            numbers[j] = numbers[j + 1];
+//            numbers[j + 1] = save;
+
+
+//            //numbers[j] += numbers[j + 1];
+//            //numbers[j + 1] = numbers[j] - numbers[j + 1];
+//            //numbers[j] -= numbers[j + 1];
+//        }
+//    }
+//}
+
+
+
+//for (int i = 0;i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
+//Console.WriteLine("Addimlarin sayi: "+step);
+
+
+
+
+
+
+
+int[] arr = { 9, 6, 7, -10, 0, 3, 1, -20 };
+
+
+int step = 0;
+for(int i = 0; i < arr.Length - 1; )
+{
+    step++;
+    if (arr[i] > arr[i + 1])
+    {
+        int save = arr[i];
+        arr[i]= arr[i + 1];
+        arr[i+1]= save;
+
+        i = 0;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+
+
+for(int i = 0;i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+
+Console.WriteLine("Addim "+step);
---
+++ MethodExample/MethodExample/Program.cs
@@ -0,0 +1,210 @@
+﻿namespace MethodExample
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //int product = 90;
+            //int discount = 10;
+            //Console.WriteLine(product*discount/100);
+
+            //int product2 = 120;
+            //int discount2 = 20;
+            //Console.WriteLine(product2*discount2/100);
+
+
+
+
+            //int num=  GetDiscount(90,10);
+            //Console.WriteLine(num);
+
+            //  Console.WriteLine(GetDiscount(120, 20));
+
+            //bool result = CheckAge(19);
+
+            //CreateFullname();
+
+
+            //GetSentence("Salam", "Gencler", "necesiz");
+            //GetSentence("Hamimiz", "Kesilecik");
+
+            //GetSentence();
+
+            //int[] arr = { 1, 2, 3, 4 };
+            //int[] arr2 = { 5, 6, 7 };
+
+            //Console.WriteLine(ArrSum(arr));
+            //Console.WriteLine(ArrSum(arr2));
+            //Console.WriteLine(ArrSum(5,6,7,8,9,10));
+
+            //Sum(1, 2);
+            //Sum(3.2, 6.7);
+            //Sum(1, 2,3,4);
+
+            //Console.WriteLine();
+
+            //Sum(1);
+            //TakeFullname();
+
+            int[] arr = { 2, 3, 4, 12, 16, 113 };
+
+            Console.WriteLine(CheckCompositeArr(arr));
+
+
+        }
+
+        public static int CheckCompositeArr(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (IsComposite(nums[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        public static bool IsComposite(int num)
+        {
+
+            for (int i = 2; i*i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+
+        //public static void TakeFullname()
+        //{
+        //    Console.WriteLine("Please enter your name:");
+        //    string name=Console.ReadLine();
+
+        //    Console.WriteLine("Please enter your surname:");
+        //    string surname = Console.ReadLine();
+
+        //    Console.WriteLine(name+" "+surname);
+
+        //}
+
+        //public static void Sum(params int[] nums)
+        //{
+        //    Console.WriteLine("3ci method");
+        //}
+        //public static void Sum(int num)
+        //{
+        //    Console.WriteLine("1ci method");
+        //}
+
+        //public static void Sum(int num, int num2=20)
+        //{
+        //    Console.WriteLine("2ci method");
+        //}
+      
+       
+        //public static void Sum(int num, int num2, int num3, int num4)
+        //{
+        //    Console.WriteLine("4ci method");
+        //}
+
+
+
+
+
+        public static void Sum(double num, double num2)
+        {
+            Console.WriteLine("2ci method");
+        }
+
+
+        public static void Sum(double num, int num2)
+        {
+            Console.WriteLine("1ci method");
+        }
+        public static void Sum(int num, double num2)
+        {
+            Console.WriteLine("1ci method");
+        }
+
+
+
+       
+
+
+
+
+        public static int ArrSum(params int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+
+
+
+
+        public static void GetSentence(params string[] words)
+        {
+            string sentence = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                sentence += words[i];
+                sentence += " ";
+            }
+
+            Console.WriteLine(sentence);
+        }
+
+
+
+
+
+
+        public static void CreateFullname( string name="yyy",string surname = "xxx")
+        {
+            Console.WriteLine(name+" "+surname);
+        }
+
+        public static bool CheckAge(int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            }
+            return false;
+           
+        }
+
+
+
+
+        public static int GetDiscount(int price, int percent)
+        {
+            
+            return price * percent / 100;
+
+        }
+
+        //public static void GetDiscount(int price,int percent)
+        //{
+        //    Console.WriteLine(price * percent / 100);
+        //}
+
+    }
+
+}
