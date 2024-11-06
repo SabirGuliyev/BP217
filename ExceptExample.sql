5afa29f996ceab5819ec0baec6a6692480351dd3
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-11-06 17:10:16 +0400

    Group by

---
+++ ExceptExample.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE ExceptExample
+
+USE ExceptExample
+
+
+
+
+CREATE TABLE Products(
+Id INT PRIMARY KEY IDENTITY,
+Name VARCHAR(200) UNIQUE NOT NULL,
+Price DECIMAL(6,2) NOT NULL
+)
+
+CREATE TABLE Sizes(
+Id INT PRIMARY KEY IDENTITY,
+Name VARCHAR(80) UNIQUE NOT NULL
+)
+
+CREATE TABLE ProductSizes(
+Id INT PRIMARY KEY IDENTITY,
+ProductId INT REFERENCES Products(Id),
+SizeId INT REFERENCES Sizes(Id),
+)
+
+
+
+SELECT p.Id, p.Name,Len(s.Name) Size FROM Products AS p
+JOIN ProductSizes AS ps
+ON p.Id=ps.ProductId
+JOIN Sizes AS s
+ON ps.SizeId=s.Id
+--GROUP BY p.Name, p.Id
+
+
+SELECT  p.Name,COUNT(s.Name) Size FROM Products AS p
+JOIN ProductSizes AS ps
+ON p.Id=ps.ProductId
+JOIN Sizes AS s
+ON ps.SizeId=s.Id
+WHERE p.Price>100
+GROUP BY p.Name
+HAVING COUNT(s.Name)>3
+
+
+
+--SELECT p.Name, s.Name AS Size FROM Products AS p
+--CROSS JOIN Sizes AS s
+--WHERE p.Price BETWEEN 40 AND 300
+
+
+
+CREATE TABLE Students(
+Id INT PRIMARY KEY IDENTITY,
+Name NVARCHAR(50) NOT NULL,
+Point INT CHECK(Point BETWEEN 0 AND 100)
+)
+
+CREATE TABLE Grades(
+Id INT PRIMARY KEY IDENTITY,
+Letter CHAR(1) UNIQUE NOT NULL,
+MinGrade INT CHECK(MinGrade BETWEEN 0 AND 91 ),
+MaxGrade INT CHECK(MaxGrade BETWEEN 50 AND 100 )
+
+)
+
+
+--non-equi join
+SELECT s.Name,g.Letter AS Grade FROM Students AS s
+JOIN Grades AS g
+ON Point BETWEEN g.MinGrade AND g.MaxGrade
+
+
+
+CREATE TABLE KHProducts(
+
+Id INT PRIMARY KEY IDENTITY,
+Name VARCHAR(200) UNIQUE NOT NULL,
+Price DECIMAL(6,2) NOT NULL
+)
+
+
+CREATE TABLE IrshadProducts(
+
+Id INT PRIMARY KEY IDENTITY,
+Name VARCHAR(200) UNIQUE NOT NULL,
+Price DECIMAL(6,2) NOT NULL
+)
+
+
+
+SELECT Name FROM KHProducts
+UNION
+SELECT Name FROM IrshadProducts
+
+
+SELECT Name FROM KHProducts
+INTERSECT
+SELECT Name FROM IrshadProducts
+
+
+
+SELECT Name FROM IrshadProducts
+EXCEPT
+SELECT Name FROM KHProducts
