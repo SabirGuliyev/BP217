a2773bdcecba8d2ad3d709df28c1095a88581fe9
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-11-16 16:47:39 +0400

    EF core

---
+++ EFCore/EFCore/DAL/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DAL
+{
+    internal class AppDbContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=MSI;database=BP217EF;trusted_connection=true;integrated security=true;encrypt=false;");
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+
+    }
+}
---
+++ EFCore/EFCore/Program.cs
@@ -0,0 +1,53 @@
+﻿using EFCore.DAL;
+using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new AppDbContext();
+
+            //Student student = new()
+            //{
+            //    Name = "Ayla",
+            //    Surname = "Kishiyeva",
+            //    Age = 30
+            //};
+
+            //context.Students.Add(student);
+
+
+            //context.SaveChanges();
+
+
+
+
+
+
+            //Student student=context.Students.FirstOrDefault(s=>s.Id==4);
+
+            //student.Name = "SpecialOne";
+
+
+            //context.Students.Update(student);
+            //context.Students.Remove(student);
+
+
+            //context.SaveChanges();
+
+
+            List<Student> students = context.Students.Where(s => s.Age < 24&& s.Name.Contains("a")).ToList();
+
+            foreach (var item in students)
+            {
+                
+                Console.WriteLine(item.Id + " " + item.Name);
+            }
+
+
+        }
+    }
+}
